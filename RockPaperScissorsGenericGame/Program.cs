38bb53ff4f188664f45141eb1a021971469d8e5d
antonshinkevich <antons.hinkevich@gmail.com>
2024-04-14 19:28:29 +0300

    Add ConsoleInput class to check user input and add some code for user input in the Main method.

--- RockPaperScissorsGenericGame/Program.cs
+++ RockPaperScissorsGenericGame/Program.cs
@@ -4,6 +4,20 @@ internal static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var input = new ConsoleInput();
+		try
+		{
+			input.CheckInputLength(args.Length);
+		}
+		catch (Exception exp)
+		{
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(exp.Message);
+            Console.ResetColor();
+            Console.Write("Here is an example of how you can run this programm from the command line: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("dotnet run Rock Spoke Paper Lizard Scissors");
+            Console.ResetColor();
+        }
     }
 }
