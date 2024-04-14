791c8c83bd91f1ee558ba580218f2978f6b206df
antonshinkevich <antons.hinkevich@gmail.com>
2024-04-14 19:28:29 +0300

    Add ConsoleInput class to check user input and add some code for user input in the Main method.

---
+++ RockPaperScissorsGenericGame/ConsoleInput.cs
@@ -0,0 +1,22 @@
+﻿namespace RockPaperScissorsGenericGame;
+
+internal class ConsoleInput
+{
+    public void CheckInputLength(int inputLength)
+    {
+        if (inputLength == 0)
+        {
+            throw new ArgumentException("The number of arguments cannot be zero. You must add at least three arguments.");
+        }
+
+        if (inputLength < 3)
+        {
+            throw new ArgumentException("The number of arguments cannot be less than three. You must add at least three arguments.");
+        }
+
+        if (inputLength % 2 == 0)
+        {
+            throw new ArgumentException("The number of arguments cannot be even. Use an odd number of arguments, such as 3, 5, 7, or 21.");
+        }
+    }
+}
