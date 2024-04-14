9b4e615301afa4c18636bcbed1eff6fc38da6abc
antonshinkevich <antons.hinkevich@gmail.com>
2024-04-14 19:45:25 +0300

    Add GameMove class and mock command output

---
+++ RockPaperScissorsGenericGame/GameMove.cs
@@ -0,0 +1,18 @@
+﻿namespace RockPaperScissorsGenericGame;
+
+internal class GameMove
+{
+    public string MoveName { get; set; }
+    public int MoveIndex { get; set; }
+
+    public GameMove(string name, int index)
+    {
+        MoveName = name;
+        MoveIndex = index;
+    }
+
+    public override string ToString()
+    {
+        return $"{MoveIndex} - {MoveName}";
+    }
+}
--- RockPaperScissorsGenericGame/Program.cs
+++ RockPaperScissorsGenericGame/Program.cs
@@ -4,13 +4,32 @@ internal static class Program
 {
     static void Main(string[] args)
     {
+        CheckArguments(args);
+
+        var moves = new List<GameMove>();
+        for (int i = 0; i < args.Length; i++)
+        {
+            moves.Add(new GameMove(args[i], i + 1));
+        }
+
+        Console.WriteLine("Available moves:");
+        foreach (var gameMove in moves)
+        {
+            Console.WriteLine(gameMove);
+        }
+
+        Console.WriteLine("0 - exit\r\n? - help\r\n");
+    }
+
+    private static void CheckArguments(string[] args)
+    {
         var input = new ConsoleInput();
-		try
-		{
-			input.CheckInputLength(args.Length);
-		}
-		catch (Exception exp)
-		{
+        try
+        {
+            input.CheckInputLength(args.Length);
+        }
+        catch (Exception exp)
+        {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(exp.Message);
             Console.ResetColor();
