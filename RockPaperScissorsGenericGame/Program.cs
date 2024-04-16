3c965ff31ac69300d9842c4c31e093817e2825c9
antonshinkevich <antons.hinkevich@gmail.com>
2024-04-16 18:26:28 +0300

    Add the InputValidation class and put the methods from Program.cs there. Add validation for repeating arguments from input.

--- RockPaperScissorsGenericGame/Program.cs
+++ RockPaperScissorsGenericGame/Program.cs
@@ -6,7 +6,7 @@ internal static class Program
     {
         while (true)
         {
-            CheckArguments(ref args);
+            InputValidation.CheckArguments(ref args);
             var movesService = new GameMoveService(args);
             var matrixService = new MovesMatrix(args.Length);
             var hmacService = new HmacCalculatorService();
@@ -20,15 +20,16 @@ internal static class Program
             movesService.PrintAllMoves();
             Console.WriteLine("* - take user arguments from the console\r\n? - help\r\n0 - exit\r\n");
 
-            var userMoveValue = GetUserMove(args.Length + 1);
+            var userMoveValue = InputValidation.GetUserMove(args.Length + 1);
             if (userMoveValue == -1)
             {
                 ConsoleOutput.PrintHelp(args.Length, args, matrixService.Matrix);
                 continue;
             }
+
             if (userMoveValue == -2)
             {
-                UserArgsInput(ref args);
+                InputValidation.UserArgsInput(ref args);
                 continue;
             }
 
@@ -57,69 +58,5 @@ internal static class Program
         }
 
         ConsoleOutput.PrintHint("Good buy!");
-    }
-    private static int GetUserMove(int maxValue)
-    {
-        Console.Write("Enter your move: ");
-        var input = Console.ReadLine();
-        int number = 0;
-        var flag = input == "?" || input == "*";
-        if (flag || int.TryParse(input, out number))
-        {
-            if (input == "?") return -1;
-            if (input == "*") return -2;
-            if (number == 0) return 0;
-            if (number > 0 && number <= maxValue) return number;
-        }
-
-        ConsoleOutput.PrintError("Your input was incorrect. The input must be a number and cannot be an empty string.");
-        Console.WriteLine("Please, inter integer number or '?' sign to get a reference table with moves.");
-        ConsoleOutput.PrintHint("Available moves are 1 to " + maxValue);
-        return GetUserMove(maxValue);
-    }
-
-    private static void CheckArguments(ref string[] args)
-    {
-        ErrorCode code = GetErrorCode(args);
-        switch (code)
-        {
-            case ErrorCode.Correct:
-                return;
-            case ErrorCode.LessThanThree:
-                ConsoleOutput.PrintError("The number of arguments cannot less than three. " +
-                        "You must add at least three arguments.");
-                Console.Write("Here is an example of how you can run this programm from the command line next time: ");
-                ConsoleOutput.PrintHint("dotnet run Rock Spoke Paper Lizard Scissors");
-                UserArgsInput(ref args);
-                return;
-
-            case ErrorCode.EvenAndLong:
-                ConsoleOutput.PrintError("The number of arguments cannot be even. " +
-                        "Use an odd number of arguments, such as 3, 5, 7, or 21 for example.");
-                ConsoleOutput.PrintHint("I'll make your input shorter and remove one item. So, don't worry!");
-                args = args[..^1];
-                return; 
-        }
-    }
-
-    private static void UserArgsInput(ref string[] args)
-    {
-        Console.WriteLine("Enter your parametes using one string and space sign ' ' as a separator." +
-            "Unfortunatelly, you cannot use spacebar in your ");
-        var secondInput = Console.ReadLine();
-        var secondArgs = secondInput?.Split(' ') ?? [];
-        CheckArguments(ref secondArgs);
-        args = secondArgs;
-        return;
-    }
-
-    internal static ErrorCode GetErrorCode(string[] args)
-    {
-        if (args.Length < 3)
-        {
-            return ErrorCode.LessThanThree;
-        }
-
-        return args.Length % 2 == 1 ? ErrorCode.Correct : ErrorCode.EvenAndLong;
     }
 }
