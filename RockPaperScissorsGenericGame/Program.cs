1e6e04710d93386479e358ee63d6badbf22711ff
antonshinkevich <antons.hinkevich@gmail.com>
2024-04-16 19:34:48 +0300

    Fix the bug with incorrect args.length passing to the GetUserMove method.

--- RockPaperScissorsGenericGame/Program.cs
+++ RockPaperScissorsGenericGame/Program.cs
@@ -20,7 +20,7 @@ internal static class Program
             movesService.PrintAllMoves();
             Console.WriteLine("* - take user arguments from the console\r\n? - help\r\n0 - exit\r\n");
 
-            var userMoveValue = InputValidation.GetUserMove(args.Length + 1);
+            var userMoveValue = InputValidation.GetUserMove(args.Length);
             if (userMoveValue == -1)
             {
                 ConsoleOutput.PrintHelp(args.Length, args, matrixService.Matrix);
